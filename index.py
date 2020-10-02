1cb03407f1d1fe52634aa16f4ac9183246d86ac2
Renolin7 <63247962+Renolin7@users.noreply.github.com>
2020-10-02 18:01:44 +0530

    Update index.py

--- index.py
+++ index.py
@@ -26,7 +26,7 @@ class BotHandler:
                 par={'chat_id': chat_id, 'animation': ani[u], 'parse_mode': 'HTML'}
                 meth='sendAnimation'
                 res = requests.post(self.api_url + meth, par)
-        else :
+        elif 'hi' not in text.lower() :
              par={'chat_id': chat_id, 'animation': 'CAACAgUAAxkBAAPfX3cXDnmVYxOa7XLvKNPBMZ0t4xEAAgcAA1cSKR0oHhl84pXJtRsE', 'parse_mode': 'HTML'}
              meth='sendAnimation'
              res = requests.post(self.api_url + meth, par)
