c167804c0b46064a62e566919128b072a0601904
Renolin7 <63247962+Renolin7@users.noreply.github.com>
2020-10-02 11:40:11 +0530

    Update index.py

--- index.py
+++ index.py
@@ -18,20 +18,19 @@ class BotHandler:
         resp = requests.get(self.api_url + method, params)
         result_json = resp.json()['result']
         return result_json
-
+    
     def send_message(self, chat_id,text):
         t_day=d()
-        
+        u=random.randint(0,12)
+        par={'chat_id': chat_id, 'animation': ani[u], 'parse_mode': 'HTML'}
+        meth='sendAnimation'
+        res = requests.post(self.api_url + meth, par)
         params = {'chat_id': chat_id, 'text': text, 'parse_mode': 'HTML'}
         method = 'sendMessage'
         resp = requests.post(self.api_url + method, params)
-        if c[0]=='0' or t_day=='Sun' :
-                u=random.randint(0,12)
-                par={'chat_id': chat_id, 'animation': ani[u], 'parse_mode': 'HTML'}
-                meth='sendAnimation'
-                res = requests.post(self.api_url + meth, par)
+        if c[0]=='0' or t_day=='Sun' :  
                 return res, resp
-        
+                break
         return resp
     def send_photo(self, chat_id, text) :
         params = {'chat_id': chat_id, 'photo': text, 'parse_mode': 'HTML'}
