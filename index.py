e910a265df6c85aac7a6d605475e0e0c6a9a24a0
Renolin7 <63247962+Renolin7@users.noreply.github.com>
2020-10-02 17:27:47 +0530

    Update index.py

--- index.py
+++ index.py
@@ -26,6 +26,10 @@ class BotHandler:
                 par={'chat_id': chat_id, 'animation': ani[u], 'parse_mode': 'HTML'}
                 meth='sendAnimation'
                 res = requests.post(self.api_url + meth, par)
+        else :
+             par={'chat_id': chat_id, 'animation': 'CAACAgUAAxkBAAPYX3cUtmUvuVkpjgG3OKd61cOuMjMAAkEBAALoFSciu_YUJ1K52uYbBA', 'parse_mode': 'HTML'}
+             meth='sendAnimation'
+             res = requests.post(self.api_url + meth, par)
         params = {'chat_id': chat_id, 'text': text, 'parse_mode': 'HTML'}
         method = 'sendMessage'
         resp = requests.post(self.api_url + method, params)
