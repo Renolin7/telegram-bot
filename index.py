3206234ba984346d8879c3f4fde0b4471b20aba3
Renolin7 <63247962+Renolin7@users.noreply.github.com>
2020-10-06 23:25:02 +0530

    Update index.py

--- index.py
+++ index.py
@@ -42,10 +42,10 @@ class BotHandler:
                 par={'chat_id': chat_id, 'animation': ani[u], 'parse_mode': 'HTML'}
                 meth='sendAnimation'
                 res = requests.post(self.api_url + meth, par)
-        elif 'hi' not in text.lower() and 'pinned a message' not in text.lower() :
-             par={'chat_id': chat_id, 'animation': 'CAACAgUAAxkBAAPfX3cXDnmVYxOa7XLvKNPBMZ0t4xEAAgcAA1cSKR0oHhl84pXJtRsE', 'parse_mode': 'HTML'}
-             meth='sendAnimation'
-             res = requests.post(self.api_url + meth, par)
+        else:
+            par={'chat_id': chat_id, 'animation': 'CAACAgUAAxkBAAPfX3cXDnmVYxOa7XLvKNPBMZ0t4xEAAgcAA1cSKR0oHhl84pXJtRsE', 'parse_mode': 'HTML'}
+            meth='sendAnimation'
+            res = requests.post(self.api_url + meth, par)
         params = {'chat_id': chat_id,'reply_to_message_id' : m_id, 'text': text, 'parse_mode': 'HTML'}
         method = 'sendMessage'
         resp = requests.post(self.api_url + method, params)
